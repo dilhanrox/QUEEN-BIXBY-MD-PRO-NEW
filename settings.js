701cb26c417896fe9c60baff6a784b01c331638b
dilhanrox <164456431+dilhanrox@users.noreply.github.com>
2024-04-11 09:03:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94760243982']
+global.premium = ['94760243982']
+global.ownernomer = '94760243982'
 global.ownername = 'Dark Alpha'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
